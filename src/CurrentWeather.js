cd4c1932b5918336021a02a8c5b65c9bcf9f59b2
Dennis <dmiller1623@gmail.com>
2018-07-19 13:53:20 -0600

    Add current weather card

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -23,13 +23,12 @@ class CurrentWeather extends Component {
 	render() {
 		return(
 			<div className="weather-card">	
-				<h1>State {this.props.currCity}</h1>
-				{/* <h1>Current Weather {this.currentCondition}</h1>
-				<h1>Day {this.currentDay}</h1>
-				<h1>Current Temp {this.currentTemp}</h1>
-				<h1>Current High {this.currentHigh}</h1>
-				<h1>Current Low {this.currentLow}</h1>
-				<h1>Summary {this.forcastSummary}</h1> */}
+				<h1>State {this.props.currentCity}</h1>
+				<h1>Day {this.props.currentDay}</h1>
+				<h1>Current Temp {this.props.currentTemp}</h1>
+				<h1>Current High{this.props.high}</h1>
+				<h1>Current Low{this.props.low}</h1>
+				<h1>Summary {this.props.summary}</h1>
 			</div>
 		)
 	}
--- src/FilterWeatherData.js
+++ src/FilterWeatherData.js
@@ -1,12 +1,14 @@
 function filterWeather (data) {
 	const currentWeather = {
 		 currCity: data.current_observation.display_location.full,
-		// currday: 
-		// currTemp:
-		// highAndLow: 
-		// summary:
+		 currDay: data.forecast.simpleforecast.forecastday[0].date.weekday,
+		 currTemp: data.current_observation.temp_f,
+		 high: data.forecast.simpleforecast.forecastday[0].high.fahrenheit,
+		 low: data.forecast.simpleforecast.forecastday[0].low.fahrenheit,
+		 summary: data.forecast.txt_forecast.forecastday[0].fcttext
 	}
 	console.log(data)
+	console.log(currentWeather)
 	return currentWeather
 }
 
