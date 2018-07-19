c9fb640ca4d5cb5086d3475de95185ad76677ba3
Dennis <dmiller1623@gmail.com>
2018-07-19 12:59:41 -0600

    Add object for filter weather

---
+++ src/FilterWeatherData.js
@@ -0,0 +1,13 @@
+function filterWeather (data) {
+	const currentWeather = {
+		// currWeather: data.current_observation.display_location.full,
+		// currday: 
+		// currTemp:
+		// highAndLow: 
+		// summary:
+	}
+	console.log(data)
+	
+}
+
+export default filterWeather;
