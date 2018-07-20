f6fdec70506e22249f70caee906cae54890a9fe1
Dennis <dmiller1623@gmail.com>
2018-07-20 09:47:11 -0600

    Add list for seven hpur

--- src/SevenHours.js
+++ src/SevenHours.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './SevenHours.css'
+
 
 export default class SevenHours extends Component {
   constructor() {
@@ -8,12 +8,12 @@ export default class SevenHours extends Component {
   }
 
   render(){
-    return 
+    return (
       <li class="seven-day-display">
-        <h1 class="hour">"Hour"{this.props.time}</h1>
-        <h1 class="proj-temp">"Temp"{this.props.temp}</h1>
+        <h1 class="hour">"Hour"{this.props.hour.time}</h1>
+        <h1 class="proj-temp">"Temp"{this.props.hour.temp}</h1>
       </li>
+    )
   }
-
 }
 
