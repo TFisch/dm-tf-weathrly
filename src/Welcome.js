70a07dd5fda2901005d8c7106342fad62f2a3109
Tim Fischer <emailtimfischer@gmail.com>
2018-07-20 19:11:16 -0600

    add weather icon

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './welcome.css'
-
+import './images/sunset.svg'
 
 const Welcome = () => {
 	return (
 		<div>
+			<img class="weather-icon" src={ require('./images/sunset.svg') } />
 			<h1 class="app-title">Weathrly</h1>
 		</div>
 	)
