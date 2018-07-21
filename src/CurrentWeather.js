a897647458d0f06b3c09250d84ab129dba1ec6f5
Tim Fischer <emailtimfischer@gmail.com>
2018-07-21 15:29:26 -0600

    Change class to className

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -21,23 +21,22 @@ class CurrentWeather extends Component {
 	// }
 
 	render() {
-		return(
-			<div className="weather-card">	
-				<h1 className="current-temp">{this.props.currentTemp}</h1>
-				<h3 className="current-city">{this.props.currentCity}</h3>
-				<h2 className="current-day">{this.props.currentDay}</h2>
-				<span className="high-low">
-					<h2 className="current-high temp-span-left">{this.props.high}</h2>
-					<p className="divide">/</p>
-					<h2 className="current-low temp-span-right">{this.props.low}</h2>
-				</span>
-				<h2 className="current-summary">{this.props.summary}</h2>
-			</div>
-		)
-	}
-	isHidden() {
+		if()
+			return(
+				<div className="weather-card">	
+					<h1 className="current-temp">{this.props.currentTemp}</h1>
+					<h3 className="current-city">{this.props.currentCity}</h3>
+					<h2 className="current-day">{this.props.currentDay}</h2>
+					<span className="high-low">
+						<h2 className="current-high temp-span-left">{this.props.high}</h2>
+						<p className="divide">/</p>
+						<h2 className="current-low temp-span-right">{this.props.low}</h2>
+					</span>
+					<h2 className="current-summary">{this.props.summary}</h2>
+				</div>
+			)
+		}
 
-	}
 
 }
 
--- src/Search.js
+++ src/Search.js
@@ -12,11 +12,11 @@ export default class Search extends Component {
 
 	render() {
 		return(
-			<div class="search-wrap">	
-				<input class="location-input" type="text" value={this.state.userLocationInput} onChange={(event) => {
+			<div className="search-wrap">	
+				<input className="location-input" type="text" value={this.state.userLocationInput} onChange={(event) => {
 					this.setState( { userLocationInput: event.target.value } )
 				}}/>
-				<button class="submit" onClick= { (event) => { this.props.setLocation(this.state.userLocationInput); }}>submit</button>
+				<button className="submit" onClick= { (event) => { this.props.setLocation(this.state.userLocationInput); }}>submit</button>
 			</div>
 		)
 	}
