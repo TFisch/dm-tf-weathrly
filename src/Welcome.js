c131a573283ff68d769253f1e83d137dbf91c2a6
Tim Fischer <emailtimfischer@gmail.com>
2018-07-20 19:19:33 -0600

    Style and size banner

--- src/Welcome.js
+++ src/Welcome.js
@@ -4,7 +4,7 @@ import './images/sunset.svg'
 
 const Welcome = () => {
 	return (
-		<div>
+		<div class="banner">
 			<img class="weather-icon" src={ require('./images/sunset.svg') } />
 			<h1 class="app-title">Weathrly</h1>
 		</div>
