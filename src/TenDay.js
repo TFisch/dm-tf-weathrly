98d2b7b12f7b2b032597adcaf99b07f0f1285089
Tim Fischer <emailtimfischer@gmail.com>
2018-07-21 01:10:26 -0600

    Span ten day high low

--- src/TenDay.js
+++ src/TenDay.js
@@ -11,8 +11,10 @@ export default class TenDay extends Component {
 		return (
 			<li class="ten-day-wrapper">
 				<h1>Day {this.props.day.day}</h1>
-				<h1>High {this.props.day.high}</h1>
-				<h1>Low {this.props.day.low}</h1>
+				<span class="high-low">
+					<h1>{this.props.day.high}</h1>
+					<h1>{this.props.day.low}</h1>
+				</span>
 			</li>
 		)
 	}
