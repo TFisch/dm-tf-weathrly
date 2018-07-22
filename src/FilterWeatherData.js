cc3a0c2531eaee6f04a552ecee96c758836a64ba
Tim Fischer <emailtimfischer@gmail.com>
2018-07-21 19:06:28 -0600

    Round current temp to whole number

--- src/FilterWeatherData.js
+++ src/FilterWeatherData.js
@@ -2,7 +2,7 @@ function filterWeather (data) {
 	const currentWeather = {
 		 currCity: data.current_observation.display_location.full,
 		 currDay: data.forecast.simpleforecast.forecastday[0].date.weekday,
-		 currTemp: data.current_observation.temp_f,
+		 currTemp: Math.floor(data.current_observation.temp_f),
 		 high: data.forecast.simpleforecast.forecastday[0].high.fahrenheit,
 		 low: data.forecast.simpleforecast.forecastday[0].low.fahrenheit,
 		 summary: data.forecast.txt_forecast.forecastday[0].fcttext
@@ -11,7 +11,8 @@ function filterWeather (data) {
 	const sevenHoursRaw = data.hourly_forecast.splice(0, 7);
 
 	const sevenHours = sevenHoursRaw.reduce((sevenHours, hour) => {
-	let hourObj = {time: hour.FCTTIME.hour,
+
+		let hourObj = {time: hour.FCTTIME.hour,
 								temp: hour.temp.english
 	}
 	sevenHours.push(hourObj)
