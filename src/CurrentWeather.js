d7297884f61b9a35fd3131b1ea4fb18d7d7318a0
Tim Fischer <emailtimfischer@gmail.com>
2018-07-23 17:04:54 -0600

    Increase size of current image

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -25,17 +25,20 @@ class CurrentWeather extends Component {
 	
 			return(
 				<div className="weather-card">	
-					<img className="current-weather-icon" src={this.props.image} />
-					<h1 className="current-temp">{this.props.currentTemp}</h1>
-					<h3 className="current-city">{this.props.currentCity}</h3>
-					<h2 className="current-day">{this.props.currentDay}</h2>
-					<span className="high-low">
-						<h2 className="current-high temp-span-left">{this.props.high}</h2>
-						<p className="divide"></p>
-						<h2 className="current-low temp-span-right">{this.props.low}</h2>
-					</span>
-					<h2 className="current-summary">{this.props.summary}</h2>
-					
+					<div className="wrap-left">
+						<h1 className="current-temp">{this.props.currentTemp}</h1>
+						<h3 className="current-city">{this.props.currentCity}</h3>
+						<h2 className="current-day">{this.props.currentDay}</h2>
+						<span className="high-low">
+							<h2 className="current-high temp-span-left">{this.props.high}</h2>
+							<p className="divide"></p>
+							<h2 className="current-low temp-span-right">{this.props.low}</h2>
+						</span>
+						<h2 className="current-summary">{this.props.summary}</h2>
+					</div>
+					<div class="wrap-right">
+						<img className="current-weather-icon" src={this.props.image} />
+					</div>	
 				</div>
 
 			)
