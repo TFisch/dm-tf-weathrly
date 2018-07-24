ab2387e9ad00b926302897d59c97c10012489a5c
Tim Fischer <emailtimfischer@gmail.com>
2018-07-24 16:41:03 -0600

    Run DidMount to update suggestions

--- src/Search.js
+++ src/Search.js
@@ -1,32 +1,54 @@
 import React, { Component } from 'react';
 import './Search.css'
 import cities from './cities.js'
-const PrefixTrie = require('@timfischer/complete-me')
-
+import { PrefixTrie } from '@timfischer/complete-me';
 
 export default class Search extends Component {
 	constructor(props) {
 		super();
 		this.state = {
 			userLocationInput: props.searchedLocation,
-			// trie: new PrefixTrie()
+			prefixTrie: null,
+			suggestions: []
 	}
 
 
-	// this.state.trie.populate(cities);
-
 }
+
+
+	handleChange = e => {
+		this.setState( { userLocationInput: e.target.value });
+		this.displaySuggestions();
+	}
+
+
+	componentDidMount() {
+		const prefixTrie = new PrefixTrie();
+		prefixTrie.populate(cities.data);
+		this.setState({
+			prefixTrie
+		})
+	}
+
+	displaySuggestions(){
+		const suggestions = this.state.prefixTrie.suggest(this.state.userLocationInput);
+		console.log(suggestions);
+		this.setState({
+			suggestions
+		})
+	}
+
+
 
 	render() {
 		return(
 			<div className="search-wrap">	
-				<input className="location-input" type="text" value={this.state.userLocationInput} onChange={(event) => {
-					this.setState( { userLocationInput: event.target.value } )
-				}}/>
-				<button className="submit" onClick= { (event) => { this.props.setLocation(this.state.userLocationInput); }}>submit</button>
+				<input className="location-input" type="text" value={this.state.userLocationInput} onChange={e => this.handleChange(e)}/>
+				<button className="submit" onClick= {() =>	this.props.setLocation(this.state.userLocationInput)}>submit</button>
 			</div>
 		) 
 	}
+
 
 
 }
