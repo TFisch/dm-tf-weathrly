68975363a1823425e29e49dfd439b91f8c49bc4b
Tim Fischer <emailtimfischer@gmail.com>
2018-08-08 13:19:11 -0600

    Remove commented out code

--- src/Search.js
+++ src/Search.js
@@ -3,48 +3,59 @@ import './Search.css';
 import cities from './cities.js';
 import { PrefixTrie } from '@timfischer/complete-me';
 
-
 export default class Search extends Component {
-	constructor(props) {
-		super();
-		this.state = {
-			userLocationInput: '',
-			prefixTrie: null,
-			suggestions: []
-		};
+  constructor(props) {
+    super();
+    this.state = {
+      userLocationInput: '',
+      prefixTrie: null,
+      suggestions: []
+    };
+  }
 
-	}
+  handleChange(e) {
+    this.setState({ userLocationInput: e.target.value });
+    this.displaySuggestions(e.target.value);
+  }
 
-	handleChange(e) {
-		this.setState({userLocationInput: e.target.value});
-		this.displaySuggestions(e.target.value);
-	}
+  componentDidMount() {
+    const prefixTrie = new PrefixTrie();
+    prefixTrie.populate(cities.data);
+    this.setState({
+      prefixTrie
+    });
+  }
 
-	componentDidMount() {
-		const prefixTrie = new PrefixTrie();
-		prefixTrie.populate(cities.data);
-		this.setState({
-			prefixTrie
-		});
-	}
+  displaySuggestions(userLetters) {
+    let suggestions = this.state.prefixTrie.suggest(userLetters).splice(0, 4);
+    this.setState({ suggestions: suggestions });
+  }
 
-	displaySuggestions(userLetters){
-		// this.suggestions = this.state.userLocationInput
-		let suggestions = this.state.prefixTrie.suggest(userLetters).splice(0,4);
-		this.setState({ suggestions: suggestions });
-	}
+  render() {
+    return (
+      <div className="search-wrap">
+        <input
+          className="location-input"
+          list="sugg"
+          type="text"
+          value={this.state.userLocationInput}
+          onChange={e => this.handleChange(e)}
+        />
+        <div className="suggestList">
+          <datalist className="suggest" id="sugg">
+            {this.state.suggestions.map((suggestion, index) => (
+              <option key={index}>{suggestion}</option>
+            ))}
+          </datalist>
+        </div>
 
-	render() {
-		return(
-			<div className="search-wrap">	
-				<input className="location-input" list="sugg" type="text" value={this.state.userLocationInput} onChange={(e) => this.handleChange(e)} />
-				<div className="suggestList">
-					<datalist className="suggest" id="sugg">{this.state.suggestions.map((suggestion, index) => <option key={index}>{suggestion}</option>)}</datalist>
-				</div>
-		
-				<button className="submit" onClick= {() =>	this.props.setLocation(this.state.userLocationInput)}>submit</button>
-			</div>
-		); 
-	}
-
+        <button
+          className="submit"
+          onClick={() => this.props.setLocation(this.state.userLocationInput)}
+        >
+          submit
+        </button>
+      </div>
+    );
+  }
 }
